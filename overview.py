a4b4913afdea01a99746e923106d4d83fbca08e7
hargun3045 <hargun3045@gmail.com>
2021-10-09 20:10:02 +0530

    env.yml

--- overview.py
+++ overview.py
@@ -13,10 +13,10 @@ def app():
     s = f"""<style>@import url('http://fonts.cdnfonts.com/css/ea-font');
     <style>"""
     st.markdown(s, unsafe_allow_html=True)    
-    text = '<h1 style = "font-family: \'EA Font v1.5 by Ghettoshark\'; margin-bottom:0; margin-top:0;color: #EC5A53; font-size: 40px;">HINDSIGHT</p>'
-    st.markdown(text,unsafe_allow_html=True)
-    text = '<h1 style = "font-family: \'EA Font v1.5 by Ghettoshark\'; margin-bottom:0; margin-top:0,color: #181A18; font-size: 20px;">User Analytics</p>'
-    st.markdown(text,unsafe_allow_html=True)
+    # text = '<h1 style = "font-family: \'EA Font v1.5 by Ghettoshark\'; margin-bottom:0; margin-top:0;color: #EC5A53; font-size: 40px;">HINDSIGHT</p>'
+    # st.markdown(text,unsafe_allow_html=True)
+    # text = '<h1 style = "font-family: \'EA Font v1.5 by Ghettoshark\'; margin-bottom:0; margin-top:0,color: #181A18; font-size: 20px;">User Analytics</p>'
+    # st.markdown(text,unsafe_allow_html=True)
     # st.title('Hindsight')
     # st.header('Welcome User')
     @st.cache(persist=True, allow_output_mutation=True)
